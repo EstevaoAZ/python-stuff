753263cf8f52c47b372e87415dd8bcb207c98bf7
Estevão Azevedo <110121803+EstevaoAZ@users.noreply.github.com>
2022-10-03 09:00:10 -0300

    Add files via upload
    Some new files

---
+++ loop_for.py
@@ -0,0 +1,60 @@
+"""
+Loop for
+
+Loop for -. Estrutura de repetição.
+For -> Umas dessasd estruturas
+
+C ou Java
+
+for(int i = 0; i < limitador; i++){
+   //execução do loop
+}
+
+Python
+
+for item in interavel:
+    //execução do loop
+
+
+
+Ultilizamos loops para interar sobre sequências ou sobre valores iteráveis
+
+Exemplos de iteráveis
+ - String
+    nome = 'Estevao Azevedo'
+- Lista
+   lista = [1, 3, 5, 7, 9]
+- Range
+   numeros = range(1, 10)
+
+# Exemplo de for 1 (Iterando em uma string
+for letra in nome:
+    print(letra)
+
+# Exemplo de for 2 (Iterando sobre uma lista)
+for numero in lista:
+    print(numero)
+
+# Exemplo de for 3 (Iterando sobre um range)
+
+range(valor_inicial, valor_final)
+
+OBS: O valor final não é inclusive.
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10 - Não
+
+for numero in range(1, 10):
+    print(numero)
+"""
+
+nome = 'Estevao Azevedo'
+lista = [1, 3, 5, 7, 9]
+numeros = range(1,10)  # Temos que transformar em uma lista
